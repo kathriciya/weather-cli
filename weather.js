96b9a6b069745ac3639268cde8d49153f67fb0c4
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-18 22:31:01 +0700

    update initCLI

--- weather.js
+++ weather.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
+import { getArgs } from './helpers/args.js';
+import { printHelp } from './services/log.service.js';
 
 const initCLI = () => {
-	console.log('start');
+	const args = getArgs(process.argv);
+	console.log(args);
+	if (args.h) {
+		return printHelp();
+	}
+	if (args.s) {
+		//город
+	}
+	if (args.t) {
+		//токен
+	}
 };
 
 initCLI();
